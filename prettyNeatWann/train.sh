a01f672558ace99fc12f93c009611076ea91277b
ex7763 <eddie55020@gmail.com>
2020-06-05 16:15:14 +0800

    tmp

--- prettyNeatWann/train.sh
+++ prettyNeatWann/train.sh
@@ -4,4 +4,4 @@ sed -i 's/"task":.*/"task":"'"$1"'",/g' my_s.json
 cat my_s.json
 sed -i "s/lst = "'.*'"/lst = $2/g" domain/my_config.py
 cat domain/my_config.py
-python wann_train.py -p my_s.json -o result.out -n 8
+python wann_train.py -p my_s.json -o "$1_""`date +"%F_%H-%M-%S"`.out" -n 8
